5683340d5072102ea24bf6810cd37f9f2e611364
petute <daniel.petutschnigg@gmail.com>
2021-09-08 04:42:50 +0200

    chore: update sendgrid

--- .github/sendgrid.js
+++ .github/sendgrid.js
@@ -3,12 +3,32 @@
 const sgMail = require('@sendgrid/mail')
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
+// const fs = require('fs'),
+//   filename = 'hello-world.pdf',
+//   fileType = 'application/pdf',
+//   data = fs.readFileSync('attachments/' + filename)
+
 const msg = {
-  to: 'recipient@example.org',
-  from: 'sender@example.org',
+  to: 'kleberbaum@erebos.xyz',
+  from: 'inspiremedia-fwd@outlook.com',
   subject: 'Hello world',
   text: 'Hello plain world!',
-  html: '<p>Hello HTML world!</p>'
+  html: `<p><img src='https://topimmo.aichner.cloud/media/images/logo_topimmo.original.jpg' alt='Logo'></p> 
+    <h1>Kundenanfrage ${process.env.SUBJECT}</h1> 
+    <p><b>Projekt:</b> ${process.env.PROJECT_LINK}</p>
+    <p><b>Kunde:</b> ${process.env.FIRST_NAME} ${process.env.LAST_NAME}</p> 
+    <p><b>Kontakt E-Mail:</b> ${process.env.EMAIL}</p> 
+    <p><b>Kontakt Telefon-Nr.:</b> ${process.env.TELEPHONE}</p> 
+    <p><b>Nachricht:</b></p>
+    <p>${process.env.MESSAGE}</p>`
+  // attachments: [
+  //   {
+  //     content: data.toString('base64'),
+  //     filename: filename,
+  //     type: fileType,
+  //     disposition: 'attachment'
+  //   }
+  // ]
 }
 
 sgMail

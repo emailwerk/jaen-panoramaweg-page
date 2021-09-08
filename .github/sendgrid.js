93989aa4d10ce2fbd8ca35842e2f9939875d7820
petute <daniel.petutschnigg@gmail.com>
2021-09-08 15:58:01 +0200

    chore: update sendgrid

--- .github/sendgrid.js
+++ .github/sendgrid.js
@@ -4,7 +4,7 @@ const sgMail = require('@sendgrid/mail')
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const msg = {
-  to: 'noreply.snek.at@gmail.com',
+  to: 'kleberbaum@erebos.xyz',
   from: 'inspiremedia-fwd@outlook.com',
   subject: 'Hello world',
   text: 'Hello plain world!',
